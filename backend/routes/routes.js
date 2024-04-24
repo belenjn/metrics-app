9789d21194b8dc8728997ee138baa33322e60bc3
Belen Jaraba <belen.develop@gmail.com>
2024-04-24 12:35:03 +0200

    routes created

---
+++ backend/routes/routes.js
@@ -0,0 +1,35 @@
+module.exports = function (app, databaseService) {
+  app.get("/", (req, res) => {
+    res.json({ message: "All ok" });
+  });
+
+  app.get("/metrics", (req, res) => {
+    res.json({ message: "Reading metrics" });
+  });
+
+  app.get("/metrics/:id", async (req, res) => {
+    const metricId = req.params.id;
+    console.log(metricId);
+
+    try {
+      const metric = await databaseService.getMetricById(metricId);
+      if (metric) {
+        res.json(metric);
+      } else {
+        res.status(404).json({ message: "Metric not found" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  app.post("/metrics", async (req, res) => {
+    const newMetric = req.body;
+    try {
+      await databaseService.createMetric(newMetric);
+      res.json({ message: "New metric created" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+};
