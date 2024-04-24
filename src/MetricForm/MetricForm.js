8d19df2b28c97b22308d0130623fcedee7442014
Belen Jaraba <belen.develop@gmail.com>
2024-04-25 01:52:14 +0200

    MetricForm edited

--- src/MetricForm/MetricForm.js
+++ src/MetricForm/MetricForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import moment from "moment";
 import publishMetric from "../utils/publishMetric";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import "./MetricForm.css";
 
@@ -14,35 +17,54 @@ const MetricForm = () => {
     const timestamp = moment().format("YYYY-MM-DD HH:mm:ss");
     const metric = { timestamp, name, value };
 
-    await publishMetric(metric);
-    setName("");
-    setValue("");
+    try {
+      await publishMetric(metric);
+
+      toast.success("Metric created successfully!");
+
+      setName("");
+      setValue("");
+    } catch (error) {
+      toast.error(error);
+      console.error(error);
+    }
   };
 
   return (
     <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
+      <form onSubmit={handleSubmit} className="form">
+        <h2 className="form-title">Form to enter your values:</h2>
+
+        <div className="label-container">
+          <div className="label-container--child">
+            <label className="label">Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="form-input"
+            />
+          </div>
+
+          <br />
+          <div className="label-container--child">
+            <label className="label">Value:</label>
+            <input
+              type="number"
+              value={value}
+              onChange={(e) => setValue(parseFloat(e.target.value))}
+              required
+              className="form-input"
+            />
+          </div>
+        </div>
+
         <br />
-        <label>
-          Value:
-          <input
-            type="number"
-            value={value}
-            onChange={(e) => setValue(parseFloat(e.target.value))}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit">Publish metric</button>
+        <ToastContainer theme="dark" />
+        <button type="submit" className="submit-button">
+          Publish metric
+        </button>
       </form>
     </div>
   );
