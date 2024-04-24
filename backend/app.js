fafb1a00560a8b4ca5af33cc89705f5b88cfa665
Belen Jaraba <belen.develop@gmail.com>
2024-04-24 02:14:01 +0200

    app.js edited and functional

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-require("./routes/routes")(app);
+const { databaseService } = require("./services/databaseService");
+
+app.use(bodyParser.json());
+
+const dbService = databaseService();
+require("./routes/routes")(app, dbService);
 
 app.listen(3100, function () {
   console.log("App listening on port 3100!");
