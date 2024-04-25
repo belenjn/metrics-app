02cd143aa170a271ae8a5c5c30266e2984f9ed30
Belen Jaraba <belen.develop@gmail.com>
2024-04-25 16:56:49 +0200

    Footer added

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,12 +3,14 @@ import MetricForm from "../MetricForm/MetricForm";
 import { Metrics } from "../Metrics/Metrics";
 
 import "./MainPage.css";
+import { Footer } from "../Footer/Footer";
 
 export const MainPage = () => {
   return (
     <div className="container">
       <MetricForm />
       <Metrics />
+      <Footer />
     </div>
   );
 };
