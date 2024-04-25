bd2a011236801e0aee7f4962b3bdd3b6920e2e98
Belen Jaraba <belen.develop@gmail.com>
2024-04-25 17:38:06 +0200

    strings.js created

---
+++ src/utils/strings.js
@@ -0,0 +1,18 @@
+export const strings = {
+  formPage: {
+    title: "Form to enter your values",
+    name: "Name:",
+    value: "Value:",
+    submitButton: "Publish metric",
+    metricsButton: "Click to see metrics on the timeline",
+  },
+  metricsPage: {
+    title: "Request metrics",
+    minuteTitle: "Requests per minute",
+    hourTitle: "Requests per hour",
+    dayTitle: "Requests per day",
+  },
+  footer: {
+    name: "Belén Jaraba Navas © 2024",
+  },
+};
