08f4028123c5bf9722da4d9ba2377cbffc1ee24a
Belen Jaraba <belen.develop@gmail.com>
2024-04-25 13:12:00 +0200

    getMetrics edited

--- src/utils/getMetrics.js
+++ src/utils/getMetrics.js
@@ -1,20 +1,25 @@
 import { API_URL } from "../env";
 
 const getMetrics = async () => {
-  const response = await fetch(API_URL, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    const response = await fetch(API_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  const responseData = await response.json();
+    const responseData = await response.json();
 
-  if (response.ok) {
-    console.log("Get metrics successfully", responseData);
-    return responseData;
-  } else {
-    console.error("Error reading metric", responseData);
+    if (response.ok) {
+      return responseData;
+    } else {
+      console.error("Error fetching metrics:", responseData);
+      throw new Error("Error fetching metrics");
+    }
+  } catch (error) {
+    console.error("Error durante la solicitud de datos:", error);
+    throw error;
   }
 };
 
