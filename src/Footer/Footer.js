78b136564bbdb00097523a222487d933a5168f5d
Belen Jaraba <belen.develop@gmail.com>
2024-04-25 16:56:23 +0200

    Footer created

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <h5>Belén Jaraba Navas © 2024</h5>
+    </div>
+  );
+};
