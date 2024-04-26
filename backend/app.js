a3311d6543b499db9af828735a8c0ea76e06e793
Belen Jaraba <belen.develop@gmail.com>
2024-04-26 16:15:38 +0200

    WS configs

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
-const cors = require("cors");
 
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const { databaseService } = require("./services/databaseService");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-const { databaseService } = require("./services/databaseService");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -20,6 +23,48 @@ app.use(
 const dbService = databaseService();
 require("./routes/routes")(app, dbService);
 
-app.listen(3100, function () {
-  console.log("App listening on port 3100!");
+let clients = [];
+
+wss.on("connection", (ws) => {
+  clients.push(ws);
+
+  ws.on("close", () => {
+    clients = clients.filter((client) => client !== ws);
+  });
+});
+
+let lastCheckTimestamp = Date.now() - 3600000;
+
+async function checkForNewInsertions() {
+  try {
+    const insertions = await dbService.getRecentInsertions(lastCheckTimestamp);
+
+    if (insertions.length > 0) {
+      notifyClients(insertions);
+    }
+
+    lastCheckTimestamp = Date.now();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function notifyClients(insertions) {
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({
+          type: "NEW_INSERTIONS",
+          data: insertions,
+        })
+      );
+    }
+  });
+}
+
+const pollInterval = 5000;
+setInterval(checkForNewInsertions, pollInterval);
+
+server.listen(3100, function () {
+  console.log("3100 OK");
 });
--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { MainPage } from "./MainPage/MainPage";
+import WebSocketConnection from "./WebSocket/WebSocketConnection";
 import { MetricProvider } from "./context/MetricContext";
+import fetchMetrics from "./utils/fetchMetrics";
 
 function App() {
   return (
     <MetricProvider>
+      <WebSocketConnection fetchMetrics={fetchMetrics} />
+
       <MainPage />
     </MetricProvider>
   );
